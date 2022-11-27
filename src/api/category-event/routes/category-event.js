beeb3ad6b5741e8cafb5f2300c04fc06f5c1e16b
Olivier Bivort <olivort@hotmail.com>
2022-11-27 07:20:57 +0100

    extended data structure

---
+++ src/api/category-event/routes/category-event.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * category-event router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::category-event.category-event');
--- src/api/team/services/team.js
+++ src/api/team/services/team.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- * about service.
+ * team service
  */
 
 const { createCoreService } = require('@strapi/strapi').factories;
 
-module.exports = createCoreService('api::about.about');
+module.exports = createCoreService('api::team.team');
